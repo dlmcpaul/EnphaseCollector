82ecdb52dae0442f2a0106613a23ac3b73754b59
David McPaul <dlmcpaul@gmail.com>
2019-03-07 23:12:57 +1100

    Add assert on test

--- src/test/java/com/hz/EnphaseCollectorApplicationTests.java
+++ src/test/java/com/hz/EnphaseCollectorApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EnphaseCollectorApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
