54834567032fc6107d8a229e6579918f429bb00d
David McPaul <dlmcpaul@gmail.com>
2019-04-15 09:45:41 +1000

    Simplify the secure http client configuration

--- src/main/java/com/hz/configuration/EnphaseRestClientConfig.java
+++ src/main/java/com/hz/configuration/EnphaseRestClientConfig.java
@@ -1,34 +1,22 @@
 package com.hz.configuration;
 
-import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.auth.DigestScheme;
-import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.security.SecureRandom;
 import java.time.Duration;
 
-import static org.apache.http.auth.AuthScope.ANY_HOST;
 import static org.apache.http.auth.AuthScope.ANY_SCHEME;
 
 /**
@@ -50,8 +38,6 @@ public class EnphaseRestClientConfig {
 
     private final EnphaseCollectorProperties config;
 
-    private static SecureRandom secureRandom = new SecureRandom();
-
 	@Autowired
 	public EnphaseRestClientConfig(EnphaseCollectorProperties config) {
 		this.config = config;
@@ -61,7 +47,7 @@ public class EnphaseRestClientConfig {
 		CredentialsProvider provider = new BasicCredentialsProvider();
 		UsernamePasswordCredentials credentials =
 				new UsernamePasswordCredentials(config.getController().getUser(), config.getController().getPassword());
-		provider.setCredentials(new AuthScope(ANY_HOST, -1, REALM, ANY_SCHEME), credentials);
+		provider.setCredentials(new AuthScope(config.getController().getHost(), 80, REALM, ANY_SCHEME), credentials);
 		return provider;
 	}
 
@@ -82,51 +68,18 @@ public class EnphaseRestClientConfig {
 
 	    LOG.info("Reading from protected Envoy controller endpoint {}", config.getController().getUrl());
 
-	    HttpHost host = new HttpHost(config.getController().getHost(), config.getController().getPort(), "http");
-	    CloseableHttpClient client = HttpClientBuilder.create().
-			    setDefaultCredentialsProvider(provider()).useSystemProperties().build();
-	    HttpComponentsClientHttpRequestFactory requestFactory =
-			    new HttpComponentsClientHttpRequestFactoryDigestAuth(host, client);
+	    HttpClient httpClient = HttpClients
+			    .custom()
+			    .setDefaultCredentialsProvider(provider())
+			    .useSystemProperties()
+			    .build();
 
-	    RestTemplate template = builder.rootUri(config.getController().getUrl())
+	    return builder
+			    .rootUri(config.getController().getUrl())
 			    .setConnectTimeout(Duration.ofSeconds(5))
-			    .setReadTimeout(Duration.ofSeconds(5))
-	            .build();
-
-	    template.setRequestFactory(requestFactory);
-		return template;
+			    .setReadTimeout(Duration.ofSeconds(30))
+			    .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
+			    .build();
     }
-
-	private static class HttpComponentsClientHttpRequestFactoryDigestAuth
-			extends HttpComponentsClientHttpRequestFactory {
-
-		private final HttpHost host;
-
-		HttpComponentsClientHttpRequestFactoryDigestAuth(HttpHost host, HttpClient httpClient) {
-			super(httpClient);
-			this.host = host;
-		}
-
-		@Override
-		protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
-			return createHttpContext();
-		}
-
-		private HttpContext createHttpContext() {
-			// Create AuthCache instance
-			AuthCache authCache = new BasicAuthCache();
-			// Generate DIGEST scheme object, initialize it and add it to the local auth cache
-			DigestScheme digestScheme = new DigestScheme();
-			// If we already know the realm name
-			digestScheme.overrideParamter("realm", REALM);
-			digestScheme.overrideParamter("nonce", Long.toString(secureRandom.nextLong(), 36));
-			authCache.put(host, digestScheme);
-
-			// Add AuthCache to the execution context
-			BasicHttpContext localContext = new BasicHttpContext();
-			localContext.setAttribute(HttpClientContext.AUTH_CACHE, authCache);
-			return localContext;
-		}
-	}
 
 }
