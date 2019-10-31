5e78b671a011f101fbf18926eab51ee723c14382
David McPaul <dlmcpaul@gmail.com>
2019-11-01 10:27:38 +1100

    Mock EnvoyInfo

--- src/test/java/com/hz/EnphaseCollectorApplicationTests.java
+++ src/test/java/com/hz/EnphaseCollectorApplicationTests.java
@@ -1,7 +1,9 @@
 package com.hz;
 
+import com.hz.models.envoy.xml.EnvoyInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +14,9 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest
 @ActiveProfiles("testing")
 public class EnphaseCollectorApplicationTests {
+
+	@Mock
+	private EnvoyInfo envoyInfo;
 
 	@Test
 	public void contextLoads() {
