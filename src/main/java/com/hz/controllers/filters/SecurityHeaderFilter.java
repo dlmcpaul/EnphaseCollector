3ff13906a649651ae35fd2e155f846b58530e8a7
David McPaul <dlmcpaul@gmail.com>
2020-02-06 08:57:54 +1100

    Set some default security headers.  Note: The internal web page is not designed to be exposed on the internet

---
+++ src/main/java/com/hz/controllers/filters/SecurityHeaderFilter.java
@@ -0,0 +1,31 @@
+package com.hz.controllers.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SecurityHeaderFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse httpServletResponse=(HttpServletResponse)response;
+		httpServletResponse.setHeader("Content-Security-Policy","default-src 'self' 'unsafe-inline'");
+		httpServletResponse.setHeader("X-Frame-Options","SAMEORIGIN");
+		httpServletResponse.setHeader("X-Content-Type-Options","nosniff");
+		httpServletResponse.setHeader("Referrer-Policy","no-referrer");
+		httpServletResponse.setHeader("Feature-Policy","sync-xhr 'self'");
+		chain.doFilter(request, response);      // continue execution of other filter chain.
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+}
