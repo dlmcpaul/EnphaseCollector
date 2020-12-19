aba59588a54f8ba0c58fdf3cd6dd14f9fdba4e86
David McPaul <dlmcpaul@gmail.com>
2020-12-20 10:17:26 +1100

    Provide feedback on clicking Answer Button

--- src/main/resources/static/js/qna.js
+++ src/main/resources/static/js/qna.js
@@ -5,15 +5,20 @@ function getAnswers(target) {
 
     const form = document.getElementById(target + "-form");
 
+    const button = document.getElementById(target + "-button");
+    button.innerText = "Calculating";
+
     const request = new XMLHttpRequest();
     request.open("POST", form.action, true);
     request.responseType = "";
     request.onload = function () {
         request.onerror = function () {
             console.log("Refresh Error for " + target);
+            button.innerText = "Error";
         };
         if (request.status === 200) {
             document.getElementById(target + "-data").outerHTML = request.response;
+            button.innerText = "Answer";
         }
     };
     request.send(new FormData(form));
