4f66680d9b98a9c593c9338e09b84fc636c3f260
David McPaul <dlmcpaul@gmail.com>
2021-02-06 11:22:20 +1100

    Add some colour to version log

--- src/main/java/com/hz/components/ReleaseInfoContributor.java
+++ src/main/java/com/hz/components/ReleaseInfoContributor.java
@@ -5,6 +5,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiStyle;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -17,9 +20,12 @@ public class ReleaseInfoContributor implements InfoContributor, InitializingBean
 
 	private final Environment env;
 
+	private static final String UNRELEASED = "unreleased";
+	private static final String RELEASE_VERSION_PROPERTY = "release.version";
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		log.info("Application Release with version {}", getVersion());
+		log.info("Application Release version is {}", AnsiOutput.encode(AnsiColor.BLUE) + AnsiOutput.encode(AnsiStyle.BOLD) + getVersion() + AnsiOutput.encode(AnsiStyle.NORMAL));
 	}
 
 	@Override
@@ -29,6 +35,6 @@ public class ReleaseInfoContributor implements InfoContributor, InitializingBean
 	}
 
 	public String getVersion() {
-		return env != null && env.getProperty("release.version") != null ? env.getProperty("release.version").trim() : "unreleased";
+		return env != null && env.getProperty(RELEASE_VERSION_PROPERTY) != null ? env.getProperty(RELEASE_VERSION_PROPERTY).trim() : UNRELEASED;
 	}
 }
