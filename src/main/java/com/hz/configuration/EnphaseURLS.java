9b00ec8947a89a2b45bc57d5483f0354ab54c19f
David McPaul <dlmcpaul@gmail.com>
2023-06-08 12:55:20 +1000

    Additional URL's and documentation

--- src/main/java/com/hz/configuration/EnphaseURLS.java
+++ src/main/java/com/hz/configuration/EnphaseURLS.java
@@ -2,17 +2,19 @@ package com.hz.configuration;
 
 public class EnphaseURLS {
 
-	public static final String SYSTEM = "/home.json";
+	public static final String SYSTEM = "/home.json";   // Basic info about the envoy device
 	public static final String INVENTORY = "/inventory.json?deleted=1";
-	public static final String PRODUCTION = "/production.json?details=1";
-	public static final String CONTROLLER = "/info.xml";
+	public static final String PRODUCTION = "/production.json?details=1";   // Used for production and consumption values
+	public static final String CONTROLLER = "/info.xml";    // Used to generate passwords and detect enphase software version
 
 	// Needs Digest authentication for < V7
-	public static final String INVERTERS = "/api/v1/production/inverters";
-	public static final String WIFI_INFO = "/admin/lib/wireless_display.json?site_info=0";
+	public static final String INVERTERS = "/api/v1/production/inverters";      // Used for individual panel reads
+	public static final String WIFI_INFO = "/admin/lib/wireless_display.json?site_info=0";  // Wireless data
 	public static final String WAN_INFO = "/admin/lib/network_display.json";
 	public static final String DEVICE_METERS = "/ivp/meters";
 	public static final String POWER_METERS = "/ivp/meters/readings";
+	public static final String LIVE_STATUS = "/ivp/livedata/status";
+	public static final String DEVICE_STATUS = "/ivp/sc/status";
 
 	// Needs Installer authentication for < V7
 	public static final String METER_STREAM = "/stream/meter";
