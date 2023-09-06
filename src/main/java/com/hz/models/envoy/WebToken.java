0cb61e477a690f34257df260d987f5c8d60bcf0d
David McPaul <dlmcpaul@gmail.com>
2023-09-06 10:26:36 +1000

    More make login response more resistant to changes.

--- src/main/java/com/hz/models/envoy/WebToken.java
+++ src/main/java/com/hz/models/envoy/WebToken.java
@@ -1,9 +1,11 @@
 package com.hz.models.envoy;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WebToken {
 	@JsonProperty(value="generation_time")
 	private String generation;
