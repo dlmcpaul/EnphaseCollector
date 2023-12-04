ffe95915bb32038b7790a64c11ae877b4388d7f7
David McPaul <dlmcpaul@gmail.com>
2023-12-05 10:55:09 +1100

    Remove unneeded comment

--- src/main/java/com/hz/models/envoy/LogInResponse.java
+++ src/main/java/com/hz/models/envoy/LogInResponse.java
@@ -1,10 +1,5 @@
 package com.hz.models.envoy;
 
-// {"message":"success",
-// "session_id":"f9aebf5d9d4f11c6fce7f269075a2d6e",
-// "manager_token":"eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7InNlc3Npb25faWQiOiJmOWFlYmY1ZDlkNGYxMWM2ZmNlN2YyNjkwNzVhMmQ2ZSIsImNvbXBhbnlfaWQiOm51bGwsImVtYWlsX2lkIjoiZGxtY3BhdWxAZ21haWwuY29tIiwidXNlcl9pZCI6OTM2NDY1LCJjbGllbnRfYXBwIjoiaXRrMyIsImZpcnN0X25hbWUiOiJEYXZpZCIsImxhc3RfbmFtZSI6Ik1jUGF1bCIsImxvZ2luX3VzZXIiOm51bGwsImlzX2Rpc3RyaWJ1dG9yIjpmYWxzZX0sImV4cCI6MTY4MjI1MDk5NSwic3ViIjoiZGxtY3BhdWxAZ21haWwuY29tIn0.TS5QXls4_XmHsbeHxkFAWcLfIrJ7bUjFytLdjEgZUtE",
-// "is_consumer":true}
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
